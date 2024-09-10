d88a21f8fcc94e60c5029eaf23ac1f8aa2374749
Kshitij <kshitij234srivastava@gmail.com>
2024-09-11 03:17:58 +0530

    added remaining connections

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Auth0Provider } from '@auth0/auth0-react' 
+import {Route, Routes, Navigate } from "react-router-dom";
+
+
+import './index.css'
+import './App.css'
+import Home from './components/Home'
+import Check from './components/Check'
+import Authlogin from './auth/Authlogin'
+import Profile from './components/Profile';
+import UserWishlist from './components/UserWishList';
+import Userlist from './components/Lists'
+import Movie from './components/Movie';
+import StarRating from './components/StarRating';
+import ListMovies from './components/ListMovies';
+import Directors from './components/Directors';
+import Letmeguess from './components/Letmeguess';
+import WatchedMovies from './components/WatchedMovies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Check />} />  {/*temporary*/}
+        <Route path="/auth/login" element={<Authlogin />} />
+        <Route path="/user/profile" element={<Profile />} />
+        <Route path="/user/Wishlist" element={<UserWishlist />} />
+        <Route path="/user/List" element={<Userlist />} />
+        <Route path='/movie/:name' element={<Movie />}/>
+        <Route path='/movie/rating' element={<StarRating />}/>
+        <Route path='/user/list/:id' element={<ListMovies />}/>
+        <Route path='/user/director/:id' element={<Directors />}/>
+        <Route path='/letmeguess' element={<Letmeguess />} />
+        <Route path='/user/Watched' element={<WatchedMovies />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
