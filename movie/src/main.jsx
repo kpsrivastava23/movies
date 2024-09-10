cbe6c8d9b09c606369508469e673edc87305eb3f
Kshitij <kshitij234srivastava@gmail.com>
2024-09-11 04:34:03 +0530

    auth0 problem resolved

--- movie/src/main.jsx
+++ movie/src/main.jsx
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        {/* <Auth0Provider
-        domain="dev-rq0x1so2hesk4sy1.us.auth0.com"
-        clientId="StleRL6bpoYn2iQCSTZOS3IWRBJnDNRl"
-        authorizationParams={{
-        redirect_uri: 'https://movies-2-t3a2.onrender.com/auth/login'
-        }}
-    > */}
-        <App />
-    {/* </Auth0Provider> */}
-  </BrowserRouter>
-    
+        <Auth0Provider
+            domain="dev-rq0x1so2hesk4sy1.us.auth0.com"
+            clientId="StleRL6bpoYn2iQCSTZOS3IWRBJnDNRl"
+            authorizationParams={{
+                redirect_uri: 'https://movies-2-t3a2.onrender.com/auth/login'
+            }}
+        >
+            <App />
+        </Auth0Provider>
+    </BrowserRouter>
 )
