ae9767e7c69c2baa8df49cf6fc84cc5a140d775d
Kshitij <kshitij234srivastava@gmail.com>
2024-09-13 02:07:07 +0530

    vercel added  78

--- movie/server/database/UserLists.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const list = new mongoose.Schema({
-    email : {type : String, unique : true},
-    list_id : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lists' }]
-})
-
-const Lists = mongoose.model('User Lists', list);
-module.exports = Lists;
--- movie/server/database/Wishlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const wish = new mongoose.Schema({
-    email : {type : String, unique : true},
-    movie_id : [{type : Number}],
-})
-
-const Wishlist = mongoose.model('User Wishlists', wish);
-module.exports = Wishlist;
