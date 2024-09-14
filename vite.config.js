047cbebabc493fd523a17a7d44050865d7185ff0
Kshitij <kshitij234srivastava@gmail.com>
2024-09-15 04:40:50 +0530

    netlify

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  base: '/movies/',
   plugins: [react()],
   css: {
     postcss: {
