7b183794ac444be6d26affc9f1b28bb9ee89ce03
Kshitij <kshitij234srivastava@gmail.com>
2024-09-15 05:15:54 +0530

    last of toda2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  base: '/movies/', // Required for GitHub Pages
+  base: '/movies', // Required for GitHub Pages
   plugins: [react()],
   css: {
     postcss: {
