05c2815b45388e876e5dd82d11abe9f845d410be
Kshitij <kshitij234srivastava@gmail.com>
2024-09-15 03:40:57 +0530

    github deployment added

--- movie/vite.config.js
+++ movie/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// // import { defineConfig } from 'vite';
+// // import react from '@vitejs/plugin-react';
 
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   base: '/movies/movie/'  // Base path for GitHub Pages
+// // });
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   }
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movies/movie/'  // Base path for GitHub Pages
-});
+  base: '/movies/movie/',  // Base path for GitHub Pages
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+})
