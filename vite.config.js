64563a200b47b0dabd2c8c5c9135896767312259
Kshitij <kshitij234srivastava@gmail.com>
2024-09-15 05:03:23 +0530

    last of today

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import { defineConfig } from 'vite'; // Ensure this line exists
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
 export default defineConfig({
-  base: '/movies/', // Necessary for GitHub Pages
+  base: '/movies/', // Required for GitHub Pages
   plugins: [react()],
   css: {
     postcss: {
